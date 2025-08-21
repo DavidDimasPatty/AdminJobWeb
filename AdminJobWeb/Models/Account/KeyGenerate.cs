1ab4c7111dba695a9afa3c80b4cceec5382c96d7
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-08-21 11:49:16 +0700

    Update : Add Key Generate

---
+++ AdminJobWeb/Models/Account/KeyGenerate.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace AdminJobWeb.Models.Account
+{
+    public class KeyGenerate
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string _id { get; set; }
+        public string username { get; set; }
+        public string key { get; set; }
+        public DateTime addTime { get; set; }
+    }
+}
